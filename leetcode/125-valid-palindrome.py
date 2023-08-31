9ee9de741bd3ab5963abea079ab4959bbcb437b8
Arnav Guneta <arnavguneta@gmail.com>
2023-08-31 02:10:22 -0400

    practice

--- leetcode/125-valid-palindrome.py
+++ leetcode/125-valid-palindrome.py
@@ -1,7 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = "".join([c for c in s.lower() if (ord(c) >= 97 and ord(c) <= 122) or (ord(c) >= 48 and ord(c) <= 57)])
-        for i in range(len(s) // 2):
-            if s[i] != s[len(s)-1-i]:
+        new_s = ''
+        for c in s.lower():
+            if c.isalnum():
+                new_s += c
+        for i in range(len(new_s) // 2):
+            if new_s[i] != new_s[len(new_s)-1-i]:
                 return False
         return True
